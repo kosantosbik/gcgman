3e30379297dde2125d314c31195acf45ceae89d8
kosantosbik <312roadrunner@gmail.com>
2019-06-21 02:21:10 +0300

    push argument fix, other arguments letter option add

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gcgman",
-    version="0.0.5",
+    version="0.0.6",
     author="N.Ahmet BASTUG",
     author_email="bastugn@itu.edu.tr",
     description="A package to write any letter you want in the github graph",
